6351d6528c3c7b5eb9b4b96638f450b0a0e4d47e
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2025-05-14 11:18:44 +0530

    dev

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
     <>
       <Router>
         <Routes>
+          <Route path="/" element={<Login/>}></Route>
           <Route path="/consumer/login" element={<Login/>}></Route>
           <Route path="*" element={<ErrorPage/>}></Route>
         </Routes>
