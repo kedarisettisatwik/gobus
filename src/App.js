648577c8e2389c9967cf545b213a15046ef1a4dd
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2025-05-15 09:21:56 +0530

    dev

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Route, Routes, HashRouter as Router } from "react-router-dom";
 import CustomerLogin from "./consumer/Login";
 import DriverLogin from "./driver/Login";
-import ErrorPage from "./ErrorPage"
+// import ErrorPage from "./ErrorPage"
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/gobus/consumer/login" element={<CustomerLogin/>}></Route>
-          <Route path="/gobus/driver/login" element={<DriverLogin/>}></Route>
+          <Route path="/consumer/login" element={<CustomerLogin/>}></Route>
+          <Route path="/driver/login" element={<DriverLogin/>}></Route>
           <Route path="*" element={<CustomerLogin/>}></Route>
         </Routes>
       </Router>
