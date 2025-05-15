b5335593f3ffdd2f8b9bfd58c833141fa4f9c76c
kedarisettisatwik <74258680+kedarisettisatwik@users.noreply.github.com>
2025-05-15 09:10:25 +0530

    Dev

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes, HashRouter as Router } from "react-router-dom";
-import Login from "./consumer/Login";
+import CustomerLogin from "./consumer/Login";
+import DriverLogin from "./driver/Login";
 import ErrorPage from "./ErrorPage"
 
 function App() {
@@ -8,8 +9,9 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Login/>}></Route>
-          <Route path="/consumer/login" element={<Login/>}></Route>
+          <Route path="/gobus/consumer/login" element={<CustomerLogin/>}></Route>
+          <Route path="/gobus/driver/login" element={<DriverLogin/>}></Route>
+          <Route path="/gobus" element={<CustomerLogin/>}></Route>
           <Route path="*" element={<ErrorPage/>}></Route>
         </Routes>
       </Router>
--- src/driver/Login.js
+++ src/driver/Login.js
@@ -1,14 +1,14 @@
-import Traveling from "../stock/Traveling.png";
 import './login.css';
+import bus2 from '../stock/bus2.jpg';
 
 function Login() {
   return (
-    <section className="ConsumerLoginMain flex">
+    <section className="DriverLoginMain flex">
       <div className="img">
-        <img src={Traveling} alt="travel"></img>
+        <img src={bus2} alt="Drive"></img>
       </div>
       <div>
-        <p>Start your journey with <br></br> <i style={{color:"green",fontWeight:"bold"}}>G</i><i style={{color:"orange",fontWeight:"bold"}}>O</i> Bus</p>
+        <p>Buckle up and Start a Safe Journey! <br></br><i style={{color:"green",fontWeight:"bold"}}>G</i><i style={{color:"orange",fontWeight:"bold"}}>O</i> Bus</p>
         <span>Enter your phone number <i>*</i></span>
         <input type="tel" placeholder="78695.." pattern="[0-9]*" inputMode="numeric"></input>
         <button>Get Started</button>
